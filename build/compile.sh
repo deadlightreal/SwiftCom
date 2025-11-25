9744f1d03e784d40f9f20ba22d25ed5ecb5039b6
deadlightreal <richardfabianmain@gmail.com>
2025-11-25 22:44:26 +0100

    updated compile.sh

--- build/compile.sh
+++ build/compile.sh
@@ -1,3 +1,4 @@
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_TOOLCHAIN_FILE=/Users/admin/vcpkg/scripts/buildsystems/vcpkg.cmake .
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake .
+
 make -j8
 sudo ./output/main
